5ca77564393e496e679c5f228bd871688693b210
andyh444 <47712158+andyh444@users.noreply.github.com>
2023-10-26 21:19:52 +0100

    Add project files.

---
+++ CSharpTextEditor/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CSharpTextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new ExtendedRichTextBox();
+            highlightButton = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.AcceptsTab = true;
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Font = new Font("Consolas", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(12, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(796, 426);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "using System;\n\npublic static class Program\n{\n\tpublic static void Main()\n\t{\n\t\tConsole.WriteLine(\"Hello world\");\n\t}\n}";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            richTextBox1.KeyDown += richTextBox1_KeyDown;
+            // 
+            // highlightButton
+            // 
+            highlightButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            highlightButton.Location = new Point(12, 447);
+            highlightButton.Name = "highlightButton";
+            highlightButton.Size = new Size(122, 23);
+            highlightButton.TabIndex = 1;
+            highlightButton.Text = "Syntax Highlight";
+            highlightButton.UseVisualStyleBackColor = true;
+            highlightButton.Click += highlightButton_Click;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Location = new Point(140, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Compile and Run";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            textBox1.Location = new Point(814, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(370, 426);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1196, 482);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(highlightButton);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ExtendedRichTextBox richTextBox1;
+        private Button highlightButton;
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
---
+++ CSharpTextEditor/Form1.cs
@@ -0,0 +1,339 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpTextEditor
+{
+    public partial class Form1 : Form
+    {
+        private class TextBoxWriter : TextWriter
+        {
+            public override Encoding Encoding => Encoding.Default;
+
+            public TextBox TextBox { get; }
+
+            public TextBoxWriter(TextBox textBox)
+            {
+                TextBox = textBox;
+            }
+
+            public override void Write(string? value)
+            {
+                TextBox.Text += value;
+            }
+
+            public override void Write(char value)
+            {
+                TextBox.Text += value;
+            }
+
+            public override void WriteLine(string? value)
+            {
+                TextBox.Text += value + Environment.NewLine;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.SetOut(new TextBoxWriter(textBox1));
+        }
+
+        private void highlightButton_Click(object sender, EventArgs e)
+        {
+            HighlightSyntax();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            HighlightSyntax();
+        }
+
+        private Color GetKeywordColour(SyntaxKind syntaxKind)
+        {
+            if (syntaxKind == SyntaxKind.IfKeyword
+                || syntaxKind == SyntaxKind.ElseKeyword
+                || syntaxKind == SyntaxKind.ForEachKeyword
+                || syntaxKind == SyntaxKind.ForKeyword
+                || syntaxKind == SyntaxKind.WhileKeyword
+                || syntaxKind == SyntaxKind.DoKeyword
+                || syntaxKind == SyntaxKind.ReturnKeyword
+                || syntaxKind == SyntaxKind.TryKeyword
+                || syntaxKind == SyntaxKind.CatchKeyword
+                || syntaxKind == SyntaxKind.FinallyKeyword)
+            {
+                return Color.Purple;
+            }
+            return Color.Blue;
+        }
+
+        private void HighlightSyntax()
+        {
+            richTextBox1.BeginUpdate();
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(richTextBox1.Text);
+            var compilation = CSharpCompilation.Create("MyCompilation")
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // Reference to mscorlib
+                               MetadataReference.CreateFromFile(typeof(Console).Assembly.Location))   // Reference to System.Console)
+                .AddSyntaxTrees(tree);
+            var semanticModel = compilation.GetSemanticModel(tree);
+
+            int cursorPosition = richTextBox1.SelectionStart;
+            IEnumerable<SyntaxToken> tokens = tree.GetRoot().DescendantTokens().ToList();
+            var trivia = tree.GetRoot().DescendantTrivia().ToList();
+            HashSet<(TextSpan, Color)> currentHighlight = new HashSet<(TextSpan, Color)>();
+            foreach (var token in tokens)
+            {
+                if (token.IsKeyword())
+                {
+                    currentHighlight.Add((token.Span, GetKeywordColour(token.Kind())));
+                    //HighlightSyntax(token.Span, Color.Blue);
+                }
+                if (token.IsKind(SyntaxKind.StringLiteralToken)
+                    || token.IsKind(SyntaxKind.InterpolatedStringStartToken)
+                    || token.IsKind(SyntaxKind.InterpolatedStringTextToken)
+                    || token.IsKind(SyntaxKind.InterpolatedStringEndToken))
+                {
+                    HighlightSyntax(token.Span, Color.DarkRed);
+                    currentHighlight.Add((token.Span, Color.DarkRed));
+                }
+                if (token.IsVerbatimIdentifier())
+                {
+                    Debugger.Break();
+                }
+            }
+            foreach (var trivium in tree.GetRoot().DescendantTrivia())
+            {
+                if (trivium.IsKind(SyntaxKind.SingleLineCommentTrivia)
+                    || trivium.IsKind(SyntaxKind.MultiLineCommentTrivia))
+                {
+                    //HighlightSyntax(trivium.Span, Color.Green);
+                    currentHighlight.Add((trivium.Span, Color.Green));
+                }
+            }
+            SyntaxHighlighter h = new SyntaxHighlighter(tree, (s, c) => currentHighlight.Add((s, c)));
+            h.Visit(tree.GetRoot());
+            foreach (var diagnostic in tree.GetDiagnostics())
+            {
+                //richTextBox1.Select(diagnostic.Location.SourceSpan.Start, diagnostic.Location.SourceSpan.Length);
+                //richTextBox1.SelectionColor = Color.Red;
+                //currentHighlight.Add((diagnostic.Location.SourceSpan, Color.Red));
+            }
+
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+            foreach ((TextSpan span, Color colour) in currentHighlight)
+            {
+                HighlightSyntax(span, colour);
+            }
+            //previousHighlight = highlightsToAdd;
+            richTextBox1.DeselectAll();
+            richTextBox1.Select(cursorPosition, 0);
+            richTextBox1.EndUpdate();
+        }
+
+        private void HighlightSyntax(TextSpan span, Color colour)
+        {
+            richTextBox1.DeselectAll();
+            richTextBox1.Select(span.Start, span.Length);
+            richTextBox1.SelectionColor = colour;
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int cursorPosition = richTextBox1.SelectionStart;
+                int charIndex = richTextBox1.GetFirstCharIndexOfCurrentLine();
+                StringBuilder sb = new StringBuilder(Environment.NewLine);
+                while (charIndex < richTextBox1.Text.Length
+                    && richTextBox1.Text[charIndex++] == '\t')
+                {
+                    sb.Append("\t");
+                }
+                if (GetPreviousCharOnLine(cursorPosition, out char ch)
+                    && ch == '{')
+                {
+                    sb.Append("\t");
+                }
+                string result = sb.ToString();
+                richTextBox1.Text = richTextBox1.Text.Insert(cursorPosition, result);
+                richTextBox1.Select(cursorPosition + result.Length - 1, 0);
+                e.Handled = true;
+            }
+        }
+
+        private bool GetPreviousCharOnLine(int position, out char ch)
+        {
+            ch = default;
+            while (--position >= 0)
+            {
+                ch = richTextBox1.Text[position];
+                //if (char.IsWhiteSpace(ch))
+                if (ch == '\t'
+                    || ch == ' ')
+                {
+                    continue;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(richTextBox1.Text);
+            var compilation = CSharpCompilation.Create("MyCompilation")
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // Reference to mscorlib
+                               MetadataReference.CreateFromFile(typeof(decimal).Assembly.Location),
+                               MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Runtime")).Location),
+                               MetadataReference.CreateFromFile(typeof(Console).Assembly.Location))   // Reference to System.Console)
+                .AddSyntaxTrees(tree);
+            var semanticModel = compilation.GetSemanticModel(tree);
+            using MemoryStream ms = new MemoryStream();
+            EmitResult emitResult = compilation.Emit(ms);
+            if (!emitResult.Success)
+            {
+                foreach (var diagnostic in emitResult.Diagnostics)
+                {
+                    Console.WriteLine(diagnostic);
+                }
+            }
+            else
+            {
+                Assembly assembly = Assembly.Load(ms.ToArray());
+                Type? type = assembly.GetType("Program");
+                if (type != null)
+                {
+                    var mainMethod = type.GetMethod("Main");
+                    if (mainMethod != null)
+                    {
+                        try
+                        {
+                            mainMethod.Invoke(null, null);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Exception found during execution: {ex.Message}, {ex}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class SyntaxHighlighter : CSharpSyntaxWalker
+    {
+        private readonly SyntaxTree syntaxTree;
+        private readonly Action<TextSpan, Color> highlightAction;
+
+        public SyntaxHighlighter(SyntaxTree syntaxTree, Action<TextSpan, Color> highlightAction)
+        {
+            this.syntaxTree = syntaxTree;
+            this.highlightAction = highlightAction;
+        }
+
+        [Obsolete]
+        public void Highlight()
+        {
+            Visit(syntaxTree.GetRoot());
+        }
+
+        public override void VisitAttribute(AttributeSyntax node)
+        {
+            base.VisitAttribute(node);
+            highlightAction(node.Name.Span, Color.SteelBlue);
+        }
+
+        public override void VisitStructDeclaration(StructDeclarationSyntax node)
+        {
+            highlightAction(node.Identifier.Span, Color.SteelBlue);
+            base.VisitStructDeclaration(node);
+        }
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            // Highlight class names
+            highlightAction(node.Identifier.Span, Color.SteelBlue);
+            if (node.BaseList != null)
+            {
+                foreach (var baseType in node.BaseList.Types)
+                {
+                    highlightAction(baseType.Span, Color.SteelBlue);
+                }
+            }
+            base.VisitClassDeclaration(node);
+        }
+
+        public override void VisitConstructorDeclaration(ConstructorDeclarationSyntax node)
+        {
+            base.VisitConstructorDeclaration(node);
+            highlightAction(node.Identifier.Span, Color.SteelBlue);
+        }
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            // Highlight method names
+            highlightAction(node.Identifier.Span, Color.FromArgb(136, 108, 64));
+            foreach (ParameterSyntax parameter in node.ParameterList.Parameters)
+            {
+                if (parameter.Type is IdentifierNameSyntax ins)
+                {
+                    highlightAction(ins.FullSpan, Color.SteelBlue);
+                }
+            }
+            base.VisitMethodDeclaration(node);
+        }
+
+        public override void VisitArgumentList(ArgumentListSyntax node)
+        {
+            base.VisitArgumentList(node);
+        }
+
+        public override void VisitArgument(ArgumentSyntax node)
+        {
+            base.VisitArgument(node);
+        }
+
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            //highlightAction(node.Expression.Span, Color.FromArgb(136, 108, 64));
+            base.VisitInvocationExpression(node);
+            if (node.Expression is MemberAccessExpressionSyntax syntax)
+            {
+                highlightAction(syntax.Name.FullSpan, Color.FromArgb(136, 108, 64));
+                //if (syntax.Expression is IdentifierNameSyntax name)
+                {
+                    //highlightAction(name.FullSpan, Color.Blue);
+                }
+
+            }
+            else if (node.Expression is IdentifierNameSyntax syntax1)
+            {
+                highlightAction(syntax1.Identifier.FullSpan, Color.FromArgb(136, 108, 64));
+            }
+        }
+
+        public override void VisitAssignmentExpression(AssignmentExpressionSyntax node)
+        {
+            base.VisitAssignmentExpression(node);
+        }
+
+        public override void VisitDeclarationExpression(DeclarationExpressionSyntax node)
+        {
+            base.VisitDeclarationExpression(node);
+        }
+
+        public override void VisitVariableDeclaration(VariableDeclarationSyntax node)
+        {
+            base.VisitVariableDeclaration(node);
+            //node.
+        }
+    }
+}
