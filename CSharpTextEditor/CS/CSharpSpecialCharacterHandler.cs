02c90a7f55ff9d71b7ae70ac29bc2fa9b44c2c19
andyh444 <47712158+andyh444@users.noreply.github.com>
2025-02-17 21:32:51 +0000

    Tidy up CSharpSpecialCharacterHandler a bit

--- CSharpTextEditor/CS/CSharpSpecialCharacterHandler.cs
+++ CSharpTextEditor/CS/CSharpSpecialCharacterHandler.cs
@@ -75,44 +75,57 @@ namespace CSharpTextEditor.CS
             if (character == '('
                 || character == ',')
             {
-                if (sourceCode.SelectionRangeCollection.Count == 1)
-                {
-                    Cursor head = sourceCode.SelectionRangeCollection.PrimarySelectionRange.Head.Clone();
-                    int originalLineNumber = head.LineNumber;
+                HandleMethodInvocationGrammar(sourceCode, codeCompletionHandler, syntaxPalette);
+            }
+        }
 
-                    // back track until the opening bracket is found
-                    bool shiftSuccess = true;
-                    int parameterIndex = 0;
-                    while (shiftSuccess
-                        && head.Line.Value.GetCharacterAtIndex(head.ColumnNumber) != '('
-                        && head.LineNumber == originalLineNumber)
+        private void HandleMethodInvocationGrammar(SourceCode sourceCode, ICodeCompletionHandler codeCompletionHandler, SyntaxPalette syntaxPalette)
+        {
+            if (sourceCode.SelectionRangeCollection.Count != 1)
+            {
+                // don't do anything for multi-caret select
+                return;
+            }
+            Cursor head = sourceCode.SelectionRangeCollection.PrimarySelectionRange.Head.Clone();
+            
+            bool shiftSuccess = BacktrackCursorToMethodStartAndCountParameters(head, out int parameterCount);
+            if (shiftSuccess)
+            {
+                // shift one more, because the characterIndex needs to be one previously for the C# syntax highlighter
+                if (head.ShiftOneCharacterToTheLeft())
+                {
+                    int characterPosition = head.GetPosition().ToCharacterIndex(sourceCode.Lines);
+                    if (characterPosition != -1)
                     {
-                        if (head.Line.Value.GetCharacterAtIndex(head.ColumnNumber) == ',')
-                        {
-                            parameterIndex++;
-                        }
-                        shiftSuccess = head.ShiftOneCharacterToTheLeft();
+                        CodeCompletionSuggestion suggestion = _syntaxHighlighter.GetSuggestionAtPosition(characterPosition, syntaxPalette);
+                        codeCompletionHandler.ShowMethodCompletion(head.GetPosition(), suggestion, parameterCount);
                     }
-                    if (shiftSuccess)
+                    else
                     {
-                        // shift one more, because the characterIndex needs to be one previously for the C# syntax highlighter
-                        if (head.ShiftOneCharacterToTheLeft())
-                        {
-                            int characterPosition = head.GetPosition().ToCharacterIndex(sourceCode.Lines);
-                            if (characterPosition != -1)
-                            {
-                                CodeCompletionSuggestion suggestion = _syntaxHighlighter.GetSuggestionAtPosition(characterPosition, syntaxPalette);
-                                codeCompletionHandler.ShowMethodCompletion(head.GetPosition(), suggestion, parameterIndex);
-                            }
-                            else
-                            {
-                                // this will currently hide the method tool tip too. Maybe we don't want that
-                                codeCompletionHandler.HideCodeCompletionForm(true);
-                            }
-                        }
+                        // this will currently hide the method tool tip too. Maybe we don't want that
+                        codeCompletionHandler.HideCodeCompletionForm(true);
                     }
                 }
             }
+        }
+
+        private static bool BacktrackCursorToMethodStartAndCountParameters(Cursor head, out int parameterIndex)
+        {
+            // back track until the opening bracket is found
+            bool shiftSuccess = true;
+            parameterIndex = 0;
+            int originalLineNumber = head.LineNumber;
+            while (shiftSuccess
+                && head.Line.Value.GetCharacterAtIndex(head.ColumnNumber) != '('
+                && head.LineNumber == originalLineNumber)
+            {
+                if (head.Line.Value.GetCharacterAtIndex(head.ColumnNumber) == ',')
+                {
+                    parameterIndex++;
+                }
+                shiftSuccess = head.ShiftOneCharacterToTheLeft();
+            }
+            return shiftSuccess;
         }
     }
 }
