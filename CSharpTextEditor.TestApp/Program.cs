fcd296f84704f39b0c962e8cc7abc5dc297a2b47
andyh444 <47712158+andyh444@users.noreply.github.com>
2025-02-17 09:47:28 +0000

    Remove separate net48 test app, make other one target both

--- CSharpTextEditor.TestApp/Program.cs
+++ CSharpTextEditor.TestApp/Program.cs
@@ -8,9 +8,14 @@ namespace CSharpTextEditor.TestApp
         [STAThread]
         static void Main()
         {
+#if NET48
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+#else
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+#endif
             Application.Run(new Form1());
         }
     }
