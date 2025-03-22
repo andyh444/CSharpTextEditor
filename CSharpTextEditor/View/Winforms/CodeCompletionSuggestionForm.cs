236dfb2583f40d4e472260a1068ab1742dc4efb1
andyh444 <47712158+andyh444@users.noreply.github.com>
2025-03-22 15:56:14 +0000

    Move icon lookup to separate class

--- CSharpTextEditor/View/Winforms/CodeCompletionSuggestionForm.cs
+++ CSharpTextEditor/View/Winforms/CodeCompletionSuggestionForm.cs
@@ -25,32 +25,11 @@ namespace CSharpTextEditor.View.Winforms
         private CodeEditorBox? editorBox;
         private State? state;
 
-        private readonly Bitmap spannerIcon;
-        private readonly Bitmap methodIcon;
-        private readonly Bitmap bracketsIcon;
-        private readonly Bitmap classIcon;
-        private readonly Bitmap interfaceIcon;
-        private readonly Bitmap fieldIcon;
-        private readonly Bitmap localIcon;
-        private readonly Bitmap structIcon;
-        private readonly Bitmap enumMemberIcon;
-        private readonly Bitmap constantIcon;
-
         protected override bool ShowWithoutActivation => false;
 
         public CodeCompletionSuggestionForm()
         {
             InitializeComponent();
-            spannerIcon = Properties.Resources.spanner;
-            methodIcon = Properties.Resources.box;
-            bracketsIcon = Properties.Resources.brackets;
-            classIcon = Properties.Resources._class;
-            interfaceIcon = Properties.Resources._interface;
-            fieldIcon = Properties.Resources.field;
-            localIcon = Properties.Resources.local;
-            structIcon = Properties.Resources._struct;
-            enumMemberIcon = Properties.Resources.enumMember;
-            constantIcon = Properties.Resources.constant;
         }
 
         protected override void OnVisibleChanged(EventArgs e)
@@ -202,7 +181,7 @@ namespace CSharpTextEditor.View.Winforms
                 var selected = GetItemAtIndex(e.Index);
                 e.DrawBackground();
                 e.DrawFocusRectangle();
-                Bitmap? icon = GetIconFromSymbolType(selected.First().SymbolType);
+                Bitmap? icon = IconCache.GetIcon(selected.First().SymbolType);
                 if (icon != null)
                 {
                     e.Graphics.DrawImage(icon, e.Bounds.Location);
@@ -212,52 +191,6 @@ namespace CSharpTextEditor.View.Winforms
                     e.Graphics.DrawString(selected.Key, e.Font ?? Font, b, new Point(e.Bounds.Location.X + 16, e.Bounds.Location.Y));
                 }
             }
-        }
-
-        private Bitmap? GetIconFromSymbolType(SymbolType symbolType)
-        {
-            Bitmap? icon = null;
-            if (symbolType == SymbolType.Property)
-            {
-                icon = spannerIcon;
-            }
-            else if (symbolType == SymbolType.Method)
-            {
-                icon = methodIcon;
-            }
-            else if (symbolType == SymbolType.Namespace)
-            {
-                icon = bracketsIcon;
-            }
-            else if (symbolType == SymbolType.Class)
-            {
-                icon = classIcon;
-            }
-            else if (symbolType == SymbolType.Interface)
-            {
-                icon = interfaceIcon;
-            }
-            else if (symbolType == SymbolType.Field)
-            {
-                icon = fieldIcon;
-            }
-            else if (symbolType == SymbolType.Local)
-            {
-                icon = localIcon;
-            }
-            else if (symbolType == SymbolType.Struct)
-            {
-                icon = structIcon;
-            }
-            else if (symbolType == SymbolType.EnumMember)
-            {
-                icon = enumMemberIcon;
-            }
-            else if (symbolType == SymbolType.Constant)
-            {
-                icon = constantIcon;
-            }
-            return icon;
         }
 
         private void toolTip1_Draw(object sender, DrawToolTipEventArgs e)
---
+++ CSharpTextEditor/View/Winforms/IconCache.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTextEditor.View.Winforms
+{
+    internal class IconCache
+    {
+        // TODO: Consider disposal
+        private Dictionary<SymbolType, Bitmap> _icons;
+        private static IconCache? instance;
+
+        private IconCache()
+        {
+            _icons = new Dictionary<SymbolType, Bitmap>();
+        }
+
+        private static IconCache Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new IconCache();
+                }
+                return instance;
+            }
+        }
+
+        public static Bitmap? GetIcon(SymbolType symbolType) => Instance.GetIconInternal(symbolType);
+
+        private Bitmap? GetIconInternal(SymbolType symbolType)
+        {
+            if (_icons.TryGetValue(symbolType, out Bitmap? cachedIcon))
+            {
+                return cachedIcon;
+            }
+            Bitmap? icon = GetIconFromResources(symbolType);
+            if (icon != null)
+            {
+                _icons.Add(symbolType, icon);
+                return icon;
+            }
+            return null;
+        }
+
+        private static Bitmap? GetIconFromResources(SymbolType symbolType)
+        {
+            Bitmap? icon = null;
+            if (symbolType == SymbolType.Property)
+            {
+                icon = Properties.Resources.spanner;
+            }
+            else if (symbolType == SymbolType.Method)
+            {
+                icon = Properties.Resources.box;
+            }
+            else if (symbolType == SymbolType.Namespace)
+            {
+                icon = Properties.Resources.brackets;
+            }
+            else if (symbolType == SymbolType.Class)
+            {
+                icon = Properties.Resources._class;
+            }
+            else if (symbolType == SymbolType.Interface)
+            {
+                icon = Properties.Resources._interface;
+            }
+            else if (symbolType == SymbolType.Field)
+            {
+                icon = Properties.Resources.field;
+            }
+            else if (symbolType == SymbolType.Local)
+            {
+                icon = Properties.Resources.local;
+            }
+            else if (symbolType == SymbolType.Struct)
+            {
+                icon = Properties.Resources._struct;
+            }
+            else if (symbolType == SymbolType.EnumMember)
+            {
+                icon = Properties.Resources.enumMember;
+            }
+            else if (symbolType == SymbolType.Constant)
+            {
+                icon = Properties.Resources.constant;
+            }
+            return icon;
+        }
+    }
+}
